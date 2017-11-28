aa408b9ca2f55e86492f2f9d1b2413e245649976
fatie <fatiebusiness@gmail.com>
2017-11-28 11:24:48 -0500

    commit from script

--- r1.cmd
+++ r1.cmd
@@ -1,10 +1,10 @@
 @echo github pushing
-cd ../../git
-cd r1
-dir
+cd c:\users\fguo\git\r1
+
+
 git add "r1.cmd"
 git commit -m "commit from script"
-git push https://github.com/fatie/R-test master
+git push https://fatie:fatiebusiness1243@github.com/fatie/R-test master
 
 @echo byebye
 
